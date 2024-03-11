df6ffb3029c04ff6d28b220a1da2b5f7c492976d
Chumak Nikita <117525795+Nikita-devel@users.noreply.github.com>
2024-03-11 23:37:27 +0100

    biblioteque

---
+++ BibForMenu/exp.py
@@ -0,0 +1,113 @@
+game_connect4 = """
+Game "Connect 4":
+
+Game Rules:
+1. Players take turns selecting a column to place their game piece.
+2. The game piece is dropped to the lowest available cell in the chosen column.
+3. The objective is to connect four of your game pieces either horizontally, vertically, or diagonally.
+
+Instructions for Playing:
+1. Your turn is determined by the order of play.
+2. Left-click to choose a column to place your game piece.
+3. The game ends when a player forms a line of four game pieces or when all cells on the board are filled.
+
+Enjoy the game!
+"""
+
+flappy_bird = """
+How to Play Flappy Bird:
+
+Game Objective:
+- Control the bird's flight by pressing the spacebar to navigate between pipes.
+- The goal is to pass through the openings between the pipes and earn points.
+
+Controls:
+- Press the spacebar to make the bird flap its wings and ascend.
+- Avoid collisions with the ground and pipes.
+
+Scoring:
+- Earn one point each time the bird successfully passes through an opening.
+- Try to achieve the highest score possible.
+
+Game Over:
+- The game ends if the bird collides with the ground or pipes.
+- After the game over, you can restart by pressing the spacebar.
+
+Exit the Game:
+- If you want to exit the game, click the "Exit" button displayed on the screen.
+
+Enjoy playing Flappy Bird!
+"""
+
+pong_game = """
+How to Play Pong:
+
+Objective:
+- Control the paddles to hit the ball back and forth.
+- Score points by making the ball pass the opponent's paddle.
+
+Controls:
+- Player A (left side): Use 'Z' to move paddle up and 'S' to move paddle down.
+- Player B (right side): Use the 'Up' arrow key to move paddle up and 'Down' arrow key to move paddle down.
+- Press 'Esc' to exit the game.
+
+Scoring:
+- A player scores when the ball passes the opponent's paddle and hits the wall behind them.
+- The game continues until one player reaches a score of 7.
+
+Winning the Game:
+- The first player to reach a score of 7 wins.
+- A message will prompt to play again or exit after determining the winner.
+
+Note:
+- The game can be restarted at any time by clicking 'yes' when prompted.
+- To exit the game at any point, press the 'Esc' key.
+
+Enjoy playing Pong!
+"""
+
+snake_game = """
+How to Play Snake Game:
+
+Objective:
+- Control the snake to eat food and grow longer.
+- Avoid collisions with the snake's own body and the game window borders.
+
+Controls:
+- Use the arrow keys (UP, DOWN, LEFT, RIGHT) to navigate the snake.
+- Press 'Q' to quit the game.
+- Press 'C' to play again after losing.
+
+Scoring:
+- The length of the snake represents the player's score.
+- Each time the snake eats food, its length increases by 1.
+- The game continues until the snake collides with itself or the window borders.
+
+Highest Score:
+- The highest score achieved in a single game is recorded.
+- The highest score is displayed along with the current score.
+
+Game Over:
+- The game ends when the snake collides with itself or the window borders.
+- A message will display the player's score and the highest score achieved.
+- Press 'C' to play again or 'Q' to quit the game.
+
+Enjoy playing Snake Game!
+"""
+
+gps_tool = """
+How to Use the GPS Data Analysis Tool:
+
+1. Click the "Démarrer" button to choose a CSV file containing GPS data.
+2. After selecting the file, a new window will appear prompting you to enter the average speed (in meters per second).
+3. Enter the average speed and click "Analyser avec la vitesse donnée" to start the analysis.
+4. The results, including total distance, positive and negative elevation changes, maximum altitude, estimated travel time, and approximate calorie expenditure, will be displayed in the text field.
+5. The analysis results can be seen in the scrolled text area.
+6. To exit the application, click the "Quitter" button.
+
+Note:
+- The tool assumes that the CSV file contains columns named "Distance (m)," "Altitude (m)," and calculates calorie expenditure using a placeholder coefficient. Adjust the coefficients as needed for accurate results.
+- In case of any errors during analysis, an error message will be displayed.
+
+Enjoy analyzing your GPS data!
+"""
